2d7a4457cc3f1addfe7b0fd825d5270b1f926cd2
leobewater <mmbee888@gmail.com>
2024-03-14 15:08:24 -0400

    add slider seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Slider;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -14,5 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
 		$this->call(UserSeeder::class);
+
+        Slider::factory(4)->create();
     }
 }
