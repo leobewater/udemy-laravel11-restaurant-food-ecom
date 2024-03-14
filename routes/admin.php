f7ea0b8ed4401056062c80681094045e39dc68ed
leobewater <mmbee888@gmail.com>
2024-03-14 10:20:42 -0400

    add admin login route

--- routes/admin.php
+++ routes/admin.php
@@ -1,16 +1,20 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminAuthController;
 use App\Http\Controllers\Admin\AdminDashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(
     [
     'prefix' => 'admin',
-    'middleware' => ['auth', 'role:admin'],
+    // 'middleware' => ['auth', 'role:admin'],
     'as' => 'admin'
     ],
     function () {
-        Route::get('dashboard', [AdminDashboardController::class,'index'])
+        // Auth Routes
+        Route::get('/login', [AdminAuthController::class,'index'])->name('admin.login');
+
+        Route::get('/dashboard', [AdminDashboardController::class,'index'])
             ->name('dashboard');
     }
 );
