8fefb13c480d03fb684cfff25a8e86029bf8b9a4
leobewater <mmbee888@gmail.com>
2024-03-13 21:10:12 -0400

    add avatar and role column to user table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,6 +18,8 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+			$table->text('avatar')->default('/uploads/avatar.png');
+			$table->enum('role', ['user', 'admin'])->default('user');
             $table->timestamps();
         });
 
