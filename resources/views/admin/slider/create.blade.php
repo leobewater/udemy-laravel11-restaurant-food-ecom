aa6d3056daed6d434d8e158f24f03aa357d33213
leobewater <mmbee888@gmail.com>
2024-03-14 15:22:42 -0400

    add slider create view in controller

---
+++ resources/views/admin/slider/create.blade.php
@@ -0,0 +1,16 @@
+@extends('admin.layouts.master')
+@section('content')
+<section class="section">
+    <div class="section-header">
+        <h1>Slider</h1>
+    </div>
+
+    <div class="card card-primary">
+        <div class="card-header">
+            <h4>Card Header</h4>
+        </div>
+        <div class="card-body">
+        </div>
+    </div>
+</section>
+@endsection
