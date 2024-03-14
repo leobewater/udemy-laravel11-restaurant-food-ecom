e46e370b76e434e4fb088b039206c7101bc50b54
leobewater <mmbee888@gmail.com>
2024-03-14 14:25:17 -0400

    avoid logged in user to visit admin/login

--- routes/admin.php
+++ routes/admin.php
@@ -8,8 +8,11 @@ use Illuminate\Support\Facades\Route;
 Route::group(
     ['prefix' => 'admin', 'as' => 'admin.'],
     function () {
-        // Auth Routes
-        Route::get('/login', [AdminAuthController::class,'index'])->name('login');
+        // For guest, this avoid regular user logged in and go to admin/login page
+        Route::middleware(['guest'])->group(function () {
+            // Auth Routes
+            Route::get('/login', [AdminAuthController::class,'index'])->name('login');
+        });
 
         // Admin Only Routes
         Route::middleware(['auth', 'role:admin'])->group(function () {
