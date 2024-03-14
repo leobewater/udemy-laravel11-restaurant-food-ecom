cd2a5449791fc610627c4bef78ea49c3ebd35b33
leobewater <mmbee888@gmail.com>
2024-03-14 09:33:55 -0400

    add toastr message after sending forgot password email

--- app/Http/Controllers/Auth/PasswordResetLinkController.php
+++ app/Http/Controllers/Auth/PasswordResetLinkController.php
@@ -36,6 +36,11 @@ class PasswordResetLinkController extends Controller
             $request->only('email')
         );
 
+        // add status message to toastr
+        if ($status == Password::RESET_LINK_SENT) {
+            toastr('Reset link sent to your email.', 'success');
+        }
+
         return $status == Password::RESET_LINK_SENT
                     ? back()->with('status', __($status))
                     : back()->withInput($request->only('email'))
