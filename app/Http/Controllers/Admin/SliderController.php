536e954ff283d4554fd2e92fb81f1ac9c9f6477f
leobewater <mmbee888@gmail.com>
2024-03-14 15:32:09 -0400

    define slider create form fields

--- app/Http/Controllers/Admin/SliderController.php
+++ app/Http/Controllers/Admin/SliderController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\DataTables\SliderDataTable;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class SliderController extends Controller
 {
@@ -19,7 +20,7 @@ class SliderController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create(): View
     {
         return view('admin.slider.create');
     }
--- resources/views/admin/slider/create.blade.php
+++ resources/views/admin/slider/create.blade.php
@@ -1,16 +1,53 @@
 @extends('admin.layouts.master')
 @section('content')
-<section class="section">
-    <div class="section-header">
-        <h1>Slider</h1>
-    </div>
+    <section class="section">
+        <div class="section-header">
+            <h1>Add Slider</h1>
+        </div>
 
-    <div class="card card-primary">
-        <div class="card-header">
-            <h4>Card Header</h4>
+        <div class="card card-primary">
+            <div class="card-header">
+                <h4>Card Header</h4>
+            </div>
+            <div class="card-body">
+                <form action="">
+                    <div class="form-group">
+                        <label>Image</label>
+                        <div id="image-preview" class="image-preview">
+                            <label for="image-upload" id="image-label">Choose File</label>
+                            <input type="file" name="image" id="image-upload" />
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label>Offer</label>
+                        <input type="text" name="offer" class="form-control" />
+                    </div>
+                    <div class="form-group">
+                        <label>Title</label>
+                        <input type="text" name="text" class="form-control" />
+                    </div>
+                    <div class="form-group">
+                        <label>Sub Title</label>
+                        <input type="text" class="form-control" />
+                    </div>
+                    <div class="form-group">
+                        <label>Short Description</label>
+                        <textarea class="form-control" name="short_description"></textarea>
+                    </div>
+                    <div class="form-group">
+                        <label>Button Link</label>
+                        <input type="text" class="form-control" />
+                    </div>
+                    <div class="form-group">
+                        <label>Status</label>
+                        <select class="form-control">
+                            <option value="1">Enable</option>
+                            <option value="0">Disable</option>
+                        </select>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Create</button>
+                </form>
+            </div>
         </div>
-        <div class="card-body">
-        </div>
-    </div>
-</section>
+    </section>
 @endsection
