a2aa5d1b62ffdd06572ec7cbc10bef47315c034a
leobewater <mmbee888@gmail.com>
2024-03-14 09:44:37 -0400

    update reset password layout and show success toaster

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,4 +1,4 @@
-<x-guest-layout>
+{{-- <x-guest-layout>
     <form method="POST" action="{{ route('password.store') }}">
         @csrf
 
@@ -36,4 +36,73 @@
             </x-primary-button>
         </div>
     </form>
-</x-guest-layout>
+</x-guest-layout> --}}
+
+@extends('frontend.layouts.master')
+@section('content')
+    <section class="fp__breadcrumb" style="background: url({{ asset('frontend/images/counter_bg.jpg') }});">
+        <div class="fp__breadcrumb_overlay">
+            <div class="container">
+                <div class="fp__breadcrumb_text">
+                    <h1>reset password</h1>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="fp__signin" style="background: url({{ asset('frontend/images/login_bg.jpg') }});">
+        <div class="fp__signin_overlay pt_125 xs_pt_95 pb_100 xs_pb_70">
+            <div class="container">
+                <div class="row wow fadeInUp" data-wow-duration="1s">
+                    <div class="col-xxl-5 col-xl-6 col-md-9 col-lg-7 m-auto">
+                        <div class="fp__login_area">
+                            <h2 class="mb-4">Reset Password!</h2>
+                            <form method="POST" action="{{ route('password.store') }}">
+                                @csrf
+                                <!-- Password Reset Token -->
+                                <input type="hidden" name="token" value="{{ $request->route('token') }}">
+
+                                <div class="row">
+                                    <div class="col-xl-12">
+                                        <div class="fp__login_imput">
+                                            <label for="email">email</label>
+                                            <input id="email" type="email" placeholder="Email" name="email"
+                                                value="{{ old('email', $request->email) }}" required />
+                                            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                                        </div>
+                                    </div>
+
+                                    <div class="col-xl-12">
+                                        <div class="fp__login_imput">
+                                            <label for="password">password</label>
+                                            <input type="password" id="password" placeholder="Password" name="password"
+                                                required>
+                                            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                                        </div>
+                                    </div>
+                                    <div class="col-xl-12">
+                                        <div class="fp__login_imput">
+                                            <label for="password_confirmation">confirm password</label>
+                                            <input type="password" id="password_confirmation" placeholder="Confirm Password"
+                                                name="password_confirmation" required>
+                                            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+                                        </div>
+                                    </div>
+
+                                    <div class="col-xl-12">
+                                        <div class="fp__login_imput">
+                                            <button type="submit" class="common_btn">Reset Password</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                            <p class="create_account d-flex justify-content-between">
+                                <a href="{{ route('login') }}">login</a>
+                                <a href="{{ route('register') }}">Create Account</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
