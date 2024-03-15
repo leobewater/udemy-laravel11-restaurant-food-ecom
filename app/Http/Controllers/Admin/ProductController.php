cd2dab1b6f2c1040ecd848521c28a6b49ff81994
leobewater <mmbee888@gmail.com>
2024-03-15 13:58:47 -0400

    add delete product behavior

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -9,7 +9,10 @@ use App\Http\Requests\Admin\ProductUpdateRequest;
 use App\Models\Category;
 use App\Models\Product;
 use App\Traits\FileUploadTrait;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\View\View;
 
 class ProductController extends Controller
@@ -19,7 +22,7 @@ class ProductController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index(ProductDataTable $dataTable)
+    public function index(ProductDataTable $dataTable): View|JsonResponse
     {
         return $dataTable->render('admin.product.index');
     }
@@ -37,7 +40,7 @@ class ProductController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(ProductCreateRequest $request)
+    public function store(ProductCreateRequest $request): RedirectResponse
     {
         $imagePath = $this->uploadImage($request, 'image');
 
@@ -88,7 +91,7 @@ class ProductController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Product $product)
+    public function edit(Product $product): View
     {
         return view('admin.product.edit', [
             'categories' => Category::all(),
@@ -99,7 +102,7 @@ class ProductController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(ProductUpdateRequest $request, Product $product)
+    public function update(ProductUpdateRequest $request, Product $product): RedirectResponse
     {
         $validatedData = $request->validated();
 
@@ -127,8 +130,24 @@ class ProductController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Product $product): Response
     {
-        //
+        try {
+            $this->removeImage($product->thumb_image);
+            $product->delete();
+
+            return response([
+                'status' => 'success',
+                'message' => 'Product deleted successfully',
+                'alert-type' => 'success'
+            ]);
+        } catch (\Exception $e) {
+            return response([
+                'status' => 'error',
+                // 'message' => $e->getMessage(),
+                'message' => 'Failed to delete item',
+                'alert-type' => 'error'
+            ]);
+        }
     }
 }
