88e2c5fce34b39e15adbee6c956d94e9e1329ef6
leobewater <mmbee888@gmail.com>
2024-03-15 10:41:31 -0400

    populate why choose us to frontend

--- app/Http/Controllers/Frontend/FrontendController.php
+++ app/Http/Controllers/Frontend/FrontendController.php
@@ -3,18 +3,35 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Models\SectionTitle;
 use App\Models\Slider;
+use App\Models\WhyChooseUs;
 use Illuminate\Http\Request;
+use Illuminate\Support\Collection;
 use Illuminate\View\View;
 
 class FrontendController extends Controller
 {
-    public function index():View
-	{
+    public function index(): View
+    {
         $sliders = Slider::where('status', 1)->get();
+        $sectionTitles = $this->getSectionTitles();
+        $WhyChooseUs = WhyChooseUs::where('status', 1)->get();
 
-		return view('frontend.home.index', [
-            'sliders' => $sliders
+        return view('frontend.home.index', [
+            'sliders' => $sliders,
+            'sectionTitles' => $sectionTitles,
+            'whyChooseUs' => $WhyChooseUs,
         ]);
-	}
+    }
+
+    private function getSectionTitles(): Collection
+    {
+        $keys = [
+            'why_choose_top_title',
+            'why_choose_main_title',
+            'why_choose_sub_title'
+        ];
+        return SectionTitle::whereIn('key', $keys)->pluck('value', 'key');
+    }
 }
