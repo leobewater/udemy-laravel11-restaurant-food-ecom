2a05920aa3882757bd1de5924f52b42906b424fc
leobewater <mmbee888@gmail.com>
2024-03-15 13:09:54 -0400

    store product

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -4,12 +4,17 @@ namespace App\Http\Controllers\Admin;
 
 use App\DataTables\ProductDataTable;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\ProductCreateRequest;
 use App\Models\Category;
+use App\Models\Product;
+use App\Traits\FileUploadTrait;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
 class ProductController extends Controller
 {
+    use FileUploadTrait;
+
     /**
      * Display a listing of the resource.
      */
@@ -31,9 +36,44 @@ class ProductController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(ProductCreateRequest $request)
     {
-        //
+        $imagePath = $this->uploadImage($request, 'image');
+
+        $validatedData = $request->validated();
+
+        Product::create([
+            ...$validatedData,
+            'thumb_image' => $imagePath,
+            'category_id' => $validatedData['category'],
+            'offer_price' => $validatedData['offer_price'] ?? 0,
+            'slug' => generateUniqueSlug('Product', $validatedData['name']),
+        ]);
+
+        /*
+        $product = new Product();
+        $product->thumb_image = $imagePath;
+        $product->name = $request->name;
+        $product->slug = generateUniqueSlug('Product', $request->name);
+        $product->category_id = $request->category;
+        $product->price = $request->price;
+        $product->offer_price = $request->offer_price ?? 0;
+        $product->quantity = $request->quantity;
+        $product->short_description = $request->short_description;
+        $product->long_description = $request->long_description;
+        $product->sku = $request->sku;
+        $product->seo_title = $request->seo_title;
+        $product->seo_description = $request->seo_description;
+        $product->show_at_home = $request->show_at_home;
+        $product->status = $request->status;
+        $product->save();
+        */
+
+        return redirect(route('admin.product.index'))->with([
+            'status' => 'created',
+            'message' => "Product created successfully",
+            'alert-type' => 'success'
+        ]);
     }
 
     /**
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'thumb_image',
+        'name',
+        'slug',
+        'category_id',
+        'price',
+        'offer_price',
+        'quantity',
+        'short_description',
+        'long_description',
+        'sku',
+        'seo_title',
+        'seo_description',
+        'show_at_home',
+        'status'
+    ];
+
 }
